4b06257d595deb074f3f661ce6b8b45f69f72a62
TaBa Trung <trungtaba@gmail.com>
2018-11-17 13:27:59 +0700

    Reverse Linklist

--- DataStructures/Linked Lists/Reverse a linked list/Solution.java
+++ DataStructures/Linked Lists/Reverse a linked list/Solution.java
@@ -1,23 +1,102 @@
-// Problem: https://www.hackerrank.com/challenges/reverse-a-linked-list
-// Java 7
-/*
-  Reverse a linked list and return pointer to the head
-  The input list will have at least one element  
-  Node is defined as  
-  class Node {
-     int data;
-     Node next;
-  }
-*/
-    // This is a "method-only" submission. 
-    // You only need to complete this method. 
-Node Reverse(Node head) {
-    Node tail = null;
-    while(head != null) {
-        Node next = head.next;
-        head.next = tail;
-        tail = head;
-        head = next;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
     }
-    return tail;
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep) {
+        while (node != null) {
+            System.out.print(node.data);
+
+            node = node.next;
+
+            if (node != null) {
+                System.out.print(sep);
+            }
+        }
+    }
+
+    // Complete the reversePrint function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static void reversePrint(SinglyLinkedListNode head) {
+        ArrayList<Integer> arrayList=new ArrayList<>();
+        while(head!=null){
+            arrayList.add(head.data);
+            head=head.next;
+        }
+        for(int i=arrayList.size()-1;i>=0;i--){
+            System.out.println(arrayList.get(i));
+        }
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int tests = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int testsItr = 0; testsItr < tests; testsItr++) {
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llistCount; i++) {
+                int llistItem = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist.insertNode(llistItem);
+            }
+
+            reversePrint(llist.head);
+        }
+
+        scanner.close();
+    }
 }
